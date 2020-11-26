5f566b92cb849c4f0c892e17761fa4ea57858002
lSenichl <baryshnikov.semen@mail.ru>
2020-11-26 21:48:02 +0300

    format

--- Lab_01/app/app.py
+++ Lab_01/app/app.py
@@ -13,14 +13,16 @@ images_ids = ['6e12f3de-d5fd-4ebb-855b-8cbc485278b7',
               'afc2cfe7-5cac-4b80-9b9a-d5c65ef0c728',
               'cab5b7f2-774e-4884-a200-0c0180fa777f']
 
+
 def generate_comments(replies=True):
     comments = []
     for i in range(randint(1, 3)):
-        comment = { 'author': fake.name(), 'text': fake.text() }
+        comment = {'author': fake.name(), 'text': fake.text()}
         if replies:
             comment['replies'] = generate_comments(replies=False)
         comments.append(comment)
     return comments
+
 
 def generate_post(i):
     return {
@@ -32,21 +34,27 @@ def generate_post(i):
         'comments': generate_comments()
     }
 
-posts_list = sorted([generate_post(i) for i in range(5)], key=lambda p: p['date'], reverse=True)
+
+posts_list = sorted([generate_post(i) for i in range(5)],
+                    key=lambda p: p['date'], reverse=True)
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/posts')
 def posts():
     title = 'Последние посты'
     return render_template('posts.html', title=title, posts=posts_list)
 
+
 @app.route('/posts/<int:index>')
 def post(index):
     p = posts_list[index]
     return render_template('post.html', title=p['title'], post=p)
+
 
 @app.route('/about')
 def about():
