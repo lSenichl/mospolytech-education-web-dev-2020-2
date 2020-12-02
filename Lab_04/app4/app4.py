63594cbf7460ad2ea80212970f4dadebce91a69b
lSenichl <baryshnikov.semen@mail.ru>
2020-12-02 23:50:02 +0300

    Lab04-2

--- Lab_04/app4/app4.py
+++ Lab_04/app4/app4.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, url_for, make_response, session, redirect, flash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
 from mysql_db import MySQL
+import mysql.connector as connector
 
 login_manager = LoginManager()
 
@@ -69,3 +70,49 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app4.route('/users')
+def users():
+    cursor = mysql.connection.cursor(named_tuple=True)
+    cursor.execute(
+        'SELECT * FROM users;')
+    users = cursor.fetchall()
+    cursor.close()
+    return render_template('users/index.html', users=users)
+
+
+@app4.route('/users/new')
+@login_required
+def new():
+    return render_template('users/new.html', user={})
+
+@app4.route('/users/create', methods=['POST'])
+@login_required
+def create():
+    login = request.form.get('login') or None
+    password = request.form.get('password')
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    middle_name = request.form.get('middle_name')
+    query = '''
+        INSERT INTO users (login, password_hash, first_name, last_name, middle_name)
+        VALUES (%s, SHA2(%s, 256), %s, %s, %s);
+    '''
+    cursor = mysql.connection.cursor(named_tuple=True)
+    try:
+        cursor.execute(query, (login, password, first_name, last_name, middle_name))
+    except connector.errors.DatabaseError:
+        flash('Введены некорректные данные, ошибка сохранения', 'danger')
+        user = {
+            'login': login,
+            'password': password,
+            'first_name': first_name,
+            'last_name': last_name,
+            'middle_name': middle_name
+        }
+        return render_template('users/new.html', user=user)
+    mysql.connection.commit()
+    cursor.close()
+    flash(f'Пользователь {login} был успешно создан.', 'success')
+    return redirect(url_for('users'))
