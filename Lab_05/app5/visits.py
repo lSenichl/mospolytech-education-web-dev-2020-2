51b7d6a83e503cee5b95413f5bba006707ef6016
lSenichl <baryshnikov.semen@mail.ru>
2020-12-21 17:10:23 +0300

    Lab05-2

--- Lab_05/app5/visits.py
+++ Lab_05/app5/visits.py
@@ -20,11 +20,28 @@ def logs():
 
 @bp.route('/stat/users')
 def users_stat():
-    records = []
+    query = '''
+    SELECT users.id, users.first_name, users.last_name, users.middle_name, count(*) AS count
+    FROM users RIGHT OUTER JOIN visit_logs ON users.id = visit_logs.user_id
+    GROUP BY users.id
+    ORDER BY count DESC;
+    '''
+    cursor = mysql.connection.cursor(named_tuple=True)
+    cursor.execute(query)
+    records = cursor.fetchall()
+    cursor.close()
     return render_template('visits/users.html', records=records)
 
 
 @bp.route('/stat/pages')
 def pages_stat():
-    records = []
+    query = '''
+    SELECT visit_logs.*, users.first_name, users.last_name, users.middle_name
+    FROM users RIGHT OUTER JOIN visit_logs ON users.id = visit_logs.user_id
+    ORDER BY visit_logs.created_at DESC;
+    '''
+    cursor = mysql.connection.cursor(named_tuple=True)
+    cursor.execute(query)
+    records = cursor.fetchall()
+    cursor.close()
     return render_template('visits/pages.html', records=records)
